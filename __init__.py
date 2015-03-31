8dde3e09f24a821f9722108ed78b5c48667d66d2
dingjun <fangdingjun@gmail.com>
2015-03-31 10:45:08 +0800

    define a rc4 function

--- __init__.py
+++ __init__.py
@@ -1,4 +1,28 @@
 # -*- coding=utf-8 -*-
+try:
+    from Crypto.Cipher import ARC4
+    def rc4(d, k):
+        a = ARC4.new(k)
+        return a.encrypt(d)
+except ImportError:
+    def rc4(string, cryptkey):
+        key_length = len(cryptkey)
+        string_length = len(string)
+        result = ''
+        rndkey = [ord(cryptkey[i % key_length]) for i in range(256)]
+        box = range(256)
+        j = 0
+        for i in xrange(256):
+            j = (j + box[i] + rndkey[i]) % 256
+            box[i], box[j] = box[j], box[i]
+
+        a, j = 0, 0
+        for i in xrange(string_length):
+            a = (a + 1) % 256
+            j = (j + box[a]) % 256
+            box[a], box[j] = box[j], box[a]
+            result += chr(ord(string[i]) ^ (box[(box[a] + box[j]) % 256]))
+        return result
 
 def now():
     import time
